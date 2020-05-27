8d7baf73348e69457e4c8f5cb492de47ffb9c299
Esiel Arizmendi <esiel_kar@hotmail.com>
2020-05-27 16:23:01 -0500

    Update RNA.java

--- src/RNA.java
+++ src/RNA.java
@@ -1,9 +1,9 @@
 public class RNA implements Comparable<RNA>{
     public static final int N_MASK = 0b1111000000000;
-    public static final int C_MASK = 0b0000110000000;
-    public static final int E_MASK = 0b0000001110000;
-    public static final int LR_MASK = 0b0000000001100;
-    public static final int M_MASK = 0b0000000000011;
+    public static final int C_MASK = 0b110000000;
+    public static final int E_MASK = 0b1110000;
+    public static final int LR_MASK = 0b1100;
+    public static final int M_MASK = 0b11;
     
     public static final int RNA_MASK = 0b1111111111111;
     
